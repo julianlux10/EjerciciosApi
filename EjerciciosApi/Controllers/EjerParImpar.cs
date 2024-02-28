ff4245ef12743a67b1dc3e23a330584daa2482b3
Julián Leiva <144271549+julianlux10@users.noreply.github.com>
2024-02-28 08:46:03 -0500

    Update EjerParImpar.cs

--- EjerciciosApi/Controllers/EjerParImpar.cs
+++ EjerciciosApi/Controllers/EjerParImpar.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EjerciciosApi.Controllersq
 {
     [Route("api/[controller]")]
     public class EjerParImparController : ControllerBase
     {
-        [HttpGet("{numero}")]
+        [HttpGet]
         public ActionResult<string> VerificarParImpar(int numero)
         {
             if (numero % 2 == 0)
@@ -17,20 +17,30 @@ namespace EjerciciosApi.Controllersq
                 return Ok($"El número {numero} es impar.");
             }
         }
-        [HttpDelete("{numero}")]
+        [HttpDelete]
         public ActionResult<string> EliminarNumero(int numero)
         {
-            return Ok($"Número {numero} eliminado correctamente.");
+            if (numero % 2 == 0) 
+            {
+                return Ok($"Número {numero} que es PAR fué eliminado correctamente.");
+            }
+            else
+            {
+                return Ok($"Número {numero} que es IMPAR fué eliminado correctamente.");
+            }
+            
         }
-        [HttpPut("{numero}")]
+        [HttpPut]
         public ActionResult<string> ActualizarNumero(int numero)
         {
-            return Ok($"Número {numero} actualizado correctamente.");
+            if(numero % 2 == 0)
+            {
+                return Ok($"Número {numero} que es PAR fué actualizado correctamente.");
+            }
+            else
+            {
+                return Ok($"Número {numero} que es IMPAR fué actualizado correctamente.");
+            }
         }
     }
 }
-
-
-
-       
-
